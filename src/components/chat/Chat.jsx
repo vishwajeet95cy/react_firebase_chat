1a23b61e3ff23245e734053c9141e476001ce56c
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-09-27 19:14:05 +0530

    feat: Chat UI Complete

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -27,7 +27,80 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              suscipit obcaecati reiciendis consectetur. Reiciendis qui ex fuga,
+              sunt asperiores ad consequatur possimus libero mollitia magni nisi
+              delectus accusamus corrupti quisquam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              suscipit obcaecati reiciendis consectetur. Reiciendis qui ex fuga,
+              sunt asperiores ad consequatur possimus libero mollitia magni nisi
+              delectus accusamus corrupti quisquam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              suscipit obcaecati reiciendis consectetur. Reiciendis qui ex fuga,
+              sunt asperiores ad consequatur possimus libero mollitia magni nisi
+              delectus accusamus corrupti quisquam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              suscipit obcaecati reiciendis consectetur. Reiciendis qui ex fuga,
+              sunt asperiores ad consequatur possimus libero mollitia magni nisi
+              delectus accusamus corrupti quisquam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              suscipit obcaecati reiciendis consectetur. Reiciendis qui ex fuga,
+              sunt asperiores ad consequatur possimus libero mollitia magni nisi
+              delectus accusamus corrupti quisquam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              suscipit obcaecati reiciendis consectetur. Reiciendis qui ex fuga,
+              sunt asperiores ad consequatur possimus libero mollitia magni nisi
+              delectus accusamus corrupti quisquam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="./img.png" alt="" />
